73a16d7cc5970485767da51a068939ea6815027b
Linus Håkansson <Linus.Hakansson@yh.nackademin.se>
2022-11-18 10:22:08 +0100

    Test Added branch so pushing works

--- src/main/java/com/skola/quizkampen/HelloApplication.java
+++ src/main/java/com/skola/quizkampen/HelloApplication.java
@@ -11,6 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        System.out.println("Hello Group! // Linus");
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
