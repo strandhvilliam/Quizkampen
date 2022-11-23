7cf59e8c119e77af1971034c30055935d70e63b7
guroSG <gustafhermans@gmail.com>
2022-11-23 09:43:37 +0100

    städat testprint

--- src/main/java/com/skola/quizkampen/HelloController.java
+++ src/main/java/com/skola/quizkampen/HelloController.java
@@ -16,38 +16,11 @@ public class HelloController {
         QuestionGenerator qg = new QuestionGenerator();
         List<Question> questionArrayList = qg.readQuestions();
 
-        System.out.println(questionArrayList.get(0).getCategory() + "\n" + questionArrayList.get(0).getQuestion() + "\n" + questionArrayList.get(0).getCorrectAnswer());
-        System.out.println(questionArrayList.get(0).getWrongAnswers()[0]);
-        System.out.println(questionArrayList.get(0).getWrongAnswers()[1]);
-        System.out.println(questionArrayList.get(0).getWrongAnswers()[2]);
-        System.out.println(questionArrayList.get(1).getCategory() + "\n" + questionArrayList.get(1).getQuestion() + "\n" + questionArrayList.get(1).getCorrectAnswer());
-        System.out.println(questionArrayList.get(1).getWrongAnswers()[0]);
-        System.out.println(questionArrayList.get(1).getWrongAnswers()[1]);
-        System.out.println(questionArrayList.get(1).getWrongAnswers()[2]);
-        System.out.println(questionArrayList.get(2).getCategory() + "\n" + questionArrayList.get(2).getQuestion() + "\n" + questionArrayList.get(2).getCorrectAnswer());
-        System.out.println(questionArrayList.get(2).getWrongAnswers()[0]);
-        System.out.println(questionArrayList.get(2).getWrongAnswers()[1]);
-        System.out.println(questionArrayList.get(2).getWrongAnswers()[2]);
-        System.out.println(questionArrayList.get(3).getCategory() + "\n" + questionArrayList.get(3).getQuestion() + "\n" + questionArrayList.get(3).getCorrectAnswer());
-        System.out.println(questionArrayList.get(3).getWrongAnswers()[0]);
-        System.out.println(questionArrayList.get(3).getWrongAnswers()[1]);
-        System.out.println(questionArrayList.get(3).getWrongAnswers()[2]);
-        System.out.println(questionArrayList.get(4).getCategory() + "\n" + questionArrayList.get(4).getQuestion() + "\n" + questionArrayList.get(4).getCorrectAnswer());
-        System.out.println(questionArrayList.get(4).getWrongAnswers()[0]);
-        System.out.println(questionArrayList.get(4).getWrongAnswers()[1]);
-        System.out.println(questionArrayList.get(4).getWrongAnswers()[2]);
-        System.out.println(questionArrayList.get(5).getCategory() + "\n" + questionArrayList.get(5).getQuestion() + "\n" + questionArrayList.get(5).getCorrectAnswer());
-        System.out.println(questionArrayList.get(5).getWrongAnswers()[0]);
-        System.out.println(questionArrayList.get(5).getWrongAnswers()[1]);
-        System.out.println(questionArrayList.get(5).getWrongAnswers()[2]);
-        System.out.println(questionArrayList.get(6).getCategory() + "\n" + questionArrayList.get(6).getQuestion() + "\n" + questionArrayList.get(6).getCorrectAnswer());
-        System.out.println(questionArrayList.get(6).getWrongAnswers()[0]);
-        System.out.println(questionArrayList.get(6).getWrongAnswers()[1]);
-        System.out.println(questionArrayList.get(6).getWrongAnswers()[2]);
-        System.out.println(questionArrayList.get(7).getCategory() + "\n" + questionArrayList.get(7).getQuestion() + "\n" + questionArrayList.get(7).getCorrectAnswer());
-        System.out.println(questionArrayList.get(7).getWrongAnswers()[0]);
-        System.out.println(questionArrayList.get(7).getWrongAnswers()[1]);
-        System.out.println(questionArrayList.get(7).getWrongAnswers()[2]);
-
+        for (int i = 0; i < 9; i++) {
+            System.out.println(questionArrayList.get(i).getCategory() + "\n" + questionArrayList.get(i).getQuestion() + "\n" + questionArrayList.get(i).getCorrectAnswer());
+            System.out.println(questionArrayList.get(i).getWrongAnswers()[0]);
+            System.out.println(questionArrayList.get(i).getWrongAnswers()[1]);
+            System.out.println(questionArrayList.get(0).getWrongAnswers()[2]);
+        }
     }
 }
