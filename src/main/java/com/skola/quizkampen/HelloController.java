15f0438d3c698973780fca61576f2645d76eacb8
guroSG <gustafhermans@gmail.com>
2022-11-23 09:08:53 +0100

    bytt till arraylista

--- src/main/java/com/skola/quizkampen/HelloController.java
+++ src/main/java/com/skola/quizkampen/HelloController.java
@@ -3,6 +3,9 @@ package com.skola.quizkampen;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
@@ -11,8 +14,11 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
         QuestionGenerator qg = new QuestionGenerator();
-        qg.readQuestions();
+        List<Question> questionArrayList = qg.readQuestions();
+
+        System.out.println(questionArrayList.get(0).getCategory() + "\n" + questionArrayList.get(0).getQuestion() + "\n" + questionArrayList.get(0).getCorrectAnswer());
+        System.out.println(questionArrayList.get(0).getWrongAnswers()[0]);
+        System.out.println(questionArrayList.get(0).getWrongAnswers()[1]);
+        System.out.println(questionArrayList.get(0).getWrongAnswers()[2]);
     }
-
-
 }
--- src/main/java/com/skola/quizkampen/QuestionGenerator.java
+++ src/main/java/com/skola/quizkampen/QuestionGenerator.java
@@ -1,55 +1,81 @@
 package com.skola.quizkampen;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class QuestionGenerator {
     private String row;
     private String question = "";
     private String rightAnswer = "";
     private String wrongAnswer = "";
-    public void readQuestions (){
+    ArrayList<Question> questionArrayList = new ArrayList<Question>();
+    private String[] wrongAnswers = new String[3];
 
+
+    private Category category;
+    public ArrayList<Question> readQuestions (){
+        int i = 0;
         try (BufferedReader br = new BufferedReader(new FileReader("D:\\javamapp\\Quizkampen\\src\\main\\java\\com\\skola\\quizkampen\\questions"))) {
             while((row = br.readLine()) != null){
-                if (row.startsWith("spel")) {
-                    int space = row.indexOf(' ');
-                    question = row.substring(space + 1);
-                    System.out.println("Fråga: " + question + "\n");
-                }
-                if (row.startsWith("geografi")) {
-                    int space = row.indexOf(' ');
-                    question = row.substring(space + 1);
-                    System.out.println("Fråga: " + question + "\n");
-                }
-                if (row.startsWith("historia")) {
-                    int space = row.indexOf(' ');
-                    question = row.substring(space + 1);
-                    System.out.println("Fråga: " + question + "\n");
-                }
-                if (row.startsWith("film")) {
-                    int space = row.indexOf(' ');
-                    question = row.substring(space + 1);
-                    System.out.println("Fråga: " + question + "\n");
-                }
-                if (row.startsWith("djur")) {
-                    int space = row.indexOf(' ');
-                    question = row.substring(space + 1);
-                    System.out.println("Fråga: " + question + "\n");
-                }
-                if (row.startsWith("teknik")) {
-                    int space = row.indexOf(' ');
-                    question = row.substring(space + 1);
-                    System.out.println("Fråga: " + question + "\n");
-                }
-                if (row.startsWith("correct")) {
-                    int space = row.indexOf(' ');
-                    rightAnswer = row.substring(space + 1);
-                    System.out.println("Rätt svar: " + rightAnswer + "\n");
-                }
-                if (row.startsWith("wrong")) {
-                    int space = row.indexOf(' ');
-                    wrongAnswer = row.substring(space + 1);
-                    System.out.println("Fel svar: " + wrongAnswer + "\n");
+                if (!row.isBlank()) {
+                    if (row.startsWith("spel")) {
+                        int space = row.indexOf(' ');
+                        question = row.substring(space + 1);
+                        //System.out.println("Spelfråga: " + question + "\n");
+                        category = Category.SPEL;
+                    }
+                    if (row.startsWith("geografi")) {
+                        int space = row.indexOf(' ');
+                        question = row.substring(space + 1);
+                        //System.out.println("Geografifråga: " + question + "\n");
+                        category = Category.GEOGRAFI;
+                    }
+                    if (row.startsWith("historia")) {
+                        int space = row.indexOf(' ');
+                        question = row.substring(space + 1);
+                        //System.out.println("Historiefråga: " + question + "\n");
+                        category = Category.HISTORIA;
+                    }
+                    if (row.startsWith("film")) {
+                        int space = row.indexOf(' ');
+                        question = row.substring(space + 1);
+                        //System.out.println("Filmfråga: " + question + "\n");
+                        category = Category.FILM;
+                    }
+                    if (row.startsWith("djur")) {
+                        int space = row.indexOf(' ');
+                        question = row.substring(space + 1);
+                        //System.out.println("Djurfråga: " + question + "\n");
+                        category = Category.DJUR;
+                    }
+                    if (row.startsWith("teknik")) {
+                        int space = row.indexOf(' ');
+                        question = row.substring(space + 1);
+                        //System.out.println("Teknikfråga: " + question + "\n");
+                        category = Category.TEKNIK;
+                    }
+                    if (row.startsWith("wrong")) {
+                        int space = row.indexOf(' ');
+                        wrongAnswer = row.substring(space + 1);
+                        wrongAnswers[i] = wrongAnswer;
+                        i++;
+
+                        //System.out.println("Fel svar: " + wrongAnswer + "\n");
+                    }
+                    if (row.startsWith("correct")) {
+                        int space = row.indexOf(' ');
+                        rightAnswer = row.substring(space + 1);
+                        //System.out.println("Rätt svar: " + rightAnswer + "\n");
+                        questionArrayList.add(new Question(question, rightAnswer, category, wrongAnswers));
+                        System.out.println(wrongAnswers[0]);
+                        System.out.println(wrongAnswers[1]);
+                        System.out.println(wrongAnswers[2]);
+                        Arrays.fill(wrongAnswers, null);
+                        i = 0;
+                    }
+                    //System.out.println(questionArray[i].getQuestion() + " " + " " + questionArray[i].getCorrectAnswer() + " " + questionArray[i].getCategory());
                 }
             }
         } catch (FileNotFoundException e) {
@@ -57,6 +83,7 @@ public class QuestionGenerator {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return questionArrayList;
     }
 }
 
