f12e9699488f408c1ce14757e38c590d8426c053
Villiam Strandh <strandh.villiam@gmail.com>
2022-11-25 16:09:27 +0100

    added fxml for Results-Window

---
+++ src/main/java/com/skola/quizkampen/ResultsController.java
@@ -0,0 +1,16 @@
+package com.skola.quizkampen;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ResultsController {
+
+    @FXML
+    private Label opponentPoints;
+
+    @FXML
+    private Label playerPoints;
+
+    @FXML
+    private Label resultsLabel;
+
+}
