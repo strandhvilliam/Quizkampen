4367b870906d913cd6adb040c05d0b7184e54593
Villiam Strandh <strandh.villiam@gmail.com>
2022-11-28 08:14:09 +0100

    various bugfixes

--- src/main/java/com/skola/quizkampen/CategoryController.java
+++ src/main/java/com/skola/quizkampen/CategoryController.java
@@ -22,7 +22,7 @@ public class CategoryController {
                 break;
             }
         }
-        clientController.displayWaitingWindow();
+       // clientController.displayWaitingWindow();
 
     }
 
